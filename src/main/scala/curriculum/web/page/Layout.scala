bf41caadea4e0e14eec385376f9e46a19c4e0ea2
Arnauld <arnauld.loyer@gmail.com>
2011-06-29 00:05:42 +0200

    Fix layout resources

--- src/main/scala/curriculum/web/page/Layout.scala
+++ src/main/scala/curriculum/web/page/Layout.scala
@@ -8,7 +8,7 @@ trait Layout {
       <head>
         <link rel="stylesheet" type="text/css" href="resources/jquery/smoothness/jquery-ui-1.8.11.custom.css"></link>
         <link rel="stylesheet" type="text/css" href="resources/curriculum.css"></link>
-        <script src="js/jquery-1.5.1.min.js" type="text/javascript"></script>
+        <script src="resources/jquery/jquery-1.5.1.min.js" type="text/javascript"></script>
         {page \ "head" \ "_"}
       </head>
       <body>
@@ -16,8 +16,8 @@ trait Layout {
         {(page \ "body" \ "_").filter(_.label != "script")}
         <!-- Load js once the page is loaded to have at least something to read in the
              meanwhile, otherwise put script in the head part to bypass this beahvior -->
-        <script src="js/jquery-ui-1.8.11.custom.min.js" type="text/javascript"></script>
-        <script src="js/curriculum.js" type="text/javascript"></script>
+        <script src="resources/jquery/jquery-ui-1.8.11.custom.min.js" type="text/javascript"></script>
+        <script src="resources/curriculum.js" type="text/javascript"></script>
         {(page \ "body" \ "_").filter(_.label == "script")}
       </body>
     </html>
